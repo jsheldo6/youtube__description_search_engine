a3638a0beefb201dacfef71ebcfddde662e73cfd
jordan Sheldon <jsheldo6@emich.edu>
2021-04-07 16:30:12 +0000

    Second commit

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request , render_template
 from youtube import search
+from description_search import create_woosh_index
 
 app = Flask(__name__) # the name of the script 
 
@@ -19,5 +20,7 @@ def query():
     if not arg or not arg.strip():
         return render_template("query.html")
 
+    index_name = "whoosh_index" + query_term
     results = search(arg,1)
+    create_woosh_index(results,index_name)
     return render_template("query.html",data=results)
---
+++ flaskr/description_search.py
@@ -0,0 +1,47 @@
+from whoosh.index import create_in, open_dir
+from whoosh.fields import Schema, TEXT,ID
+from whoosh.analysis import StemmingAnalyzer
+from whoosh.lang.porter import stem
+from whoosh.qparser import QueryParser
+from whoosh import scoring
+
+import os
+
+def create_woosh_index(video_list, index_name):
+    #Schema Defintion: video id, video title, and video description
+    schema = Schema(id=ID(stored=True),
+                    title = TEXT(stored=True),
+                    description =TEXT(analyzer=StemmingAnalyzer(),stored=True))
+    
+    if not os.path.exists(index_name):
+        os.mkdir(index_name)
+    
+    index = create_in(index_name, schema)
+    writer = index.writer()
+
+    for video_item in video_list:
+        video_id = video_item['id']
+        vide_title = video_item['snippet']['title']
+        video_description = video_item['snippet']['description']
+        writer.add_document(id=video_id, title=video_title,description = video_description)
+
+    writer.commit()
+
+
+def query_on_whoosh(index_name, query_str):
+    query_str = stem(query_str)
+    index = open_dir(index_name)
+    with index.searcher(weighting=scoring.Frequency) as searcher:
+        query = QueryParser("description", index.schema).parse(query_str)
+        results = searcher.search(query,limit=None)
+
+        formtatted_results = []
+        for result in results:
+            d = {}
+            d['url'] ="https://www.youtube.com/watch?v=" + result['id']
+            d['title'] = result['title']
+            d['description'] = result.highlights('description')
+            d['score'] = result.score
+            formatted_results.append(d)
+
+    return formatted_results
