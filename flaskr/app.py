0c833dac59abac7d2098f108410edb374f2d2eaf
jordan Sheldon <jsheldo6@emich.edu>
2021-04-27 14:24:45 +0000

    serveral issues finnished some in progress.

--- flaskr/app.py
+++ flaskr/app.py
@@ -7,13 +7,8 @@ app = Flask(__name__) # the name of the script
 # request handler
 @app.route("/")
 def index(): #method name doesnt matter to flask
-    username = request.args.get('name')
-    if not username or not username.strip():
-        username = "World"
+    return render_template("index.html")
 
-    return render_template("index.html",name=username)
-
-#100.25.129.237
 @app.route("/query", methods=['GET','POST'])
 def query():
     arg = request.args.get('q')
@@ -31,3 +26,22 @@ def query():
         search_term = request.form['description_search']
         results = query_on_whoosh(index_name, search_term)
         return render_template("query.html", query_term=arg, data=results)
+
+
+@app.route("/music", methods=['GET','POST'])
+def spotify():
+    arg = request.args.get('q')
+    if not arg or not arg.strip():
+        return render_template("music.html")
+  
+    index_name = "whoosh_index" + arg
+
+    if request.method=='GET':
+        results = search(arg,1)
+        create_woosh_index(results, index_name)
+        return render_template("music.html", query_term=arg, data=results)
+    
+    if request.method =='POST':
+        search_term = "Spotify"#request.form['description_search']
+        results = query_on_whoosh(index_name, search_term)
+        return render_template("music.html", query_term=arg, data=results)
